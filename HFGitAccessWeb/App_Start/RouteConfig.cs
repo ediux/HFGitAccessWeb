2066fb28ac4cc22b19599ddffc6ea30db519c58c
ediux <ediux@msn.com>
2016-10-31 00:44:46 +0800

    [變更] 加入GitTools工具類別庫和一個可以連接的Git對外服務連接器

--- HFGitAccessWeb/App_Start/RouteConfig.cs
+++ HFGitAccessWeb/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace HFGitAccessWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("{resource}.svc{*pathInfo}");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ HFGitAccessWeb/GitDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Services;
+using System.Data.Services.Common;
+using GitTools;
+
+public class GitDataService : DataService<GitDataSource>
+{
+    public static void InitializeService(DataServiceConfiguration config)
+    {
+        config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+        config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+    }
+}
